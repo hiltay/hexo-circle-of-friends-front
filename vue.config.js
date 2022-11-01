53547444392e6587e447907514a3b09b26dc4b5d
yyyz <904108079@qq.com>
2022-11-01 15:55:42 +0800

    Webpack_config

--- vue.config.js
+++ vue.config.js
@@ -11,6 +11,9 @@ module.exports = {
   },
   configureWebpack: {
     name: name,
+    // optimization:{
+    //   splitChunks:false
+    // },
     output: {
       // 输出重构  打包编译后的 文件名称  【模块名称.版本号.时间戳】
       filename: `[name].min.js`,
